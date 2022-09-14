8cad717e6a5f1a7640e8ad304058a6d9dc18ffd7
ManishLade <39298040+ManishLade@users.noreply.github.com>
2022-09-14 22:12:20 +0530

    Initial commit

---
+++ ShortUrl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace ShortUrl
+{
+    public class ShortUrlDto
+    {
+        public int Id { get; protected set; }
+        public string Url { get; protected set; }
+        public string UrlChunk => WebEncoders.Base64UrlEncode(BitConverter.GetBytes(Id));
+
+        public ShortUrlDto(Uri url)
+        {
+            Url = url.ToString();
+        }
+    }
+
+
+    public class UrlDto
+    {
+        public string Url { get; set; }
+
+    }
+}
